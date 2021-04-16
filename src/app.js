bc3ce6061048ab3fa03a4e25d4b6f84bdfcb6b7a
Nicolas Guillen <nicomounstro@gmail.com>
2021-04-16 13:08:18 +0000

    ejercicio terminado

--- src/app.js
+++ src/app.js
@@ -6,6 +6,24 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+  let respuesta =
+    who[getRndInteger(0, 3)] +
+    action[getRndInteger(0, 3)] +
+    what[getRndInteger(0, 2)] +
+    when[getRndInteger(0, 3)];
+  console.log(respuesta);
 };
+
+function getRndInteger(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
